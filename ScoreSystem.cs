5c23e973ef8f1bec434aa7e61590f6f5c8395009
LakshCodes <97211020+LakshCodes@users.noreply.github.com>
2022-02-04 15:13:46 -0600

    ScoreSystem updated
    Updated scoreSystem (added ad support and integrated with gameOverHandler)

--- ScoreSystem.cs
+++ ScoreSystem.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ScoreSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TMP_text scoreText;
+    [SerializeField] private float scoreMultiplier;
 
-    // Update is called once per frame
+    priavte bool shouldCount = true; 
+    private float score;
+
     void Update()
     {
-        
+        if(!shouldCount) {return;}
+        score += Time.deltaTime * scoreMultiplier;
+
+        scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+    
+    public void StartTimer()
+    {
+        shouldCount = true; 
+    }
+
+
+    public void EndTimer()
+    {
+        shouldCount = false; 
+
+        scoreText.text = string.Empty;
+
+        return Mathf.FloorToInt(score);
     }
 }

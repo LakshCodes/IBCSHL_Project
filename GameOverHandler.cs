21e849375f5060458a434f00c290afdc83396e63
LakshCodes <97211020+LakshCodes@users.noreply.github.com>
2022-02-24 19:56:18 -0600

    Update GameOverHandler.cs
    Fixed Bugs

--- GameOverHandler.cs
+++ GameOverHandler.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameOverHandler : MonoBehaviour
 {
     [SerializeField] private GameObject player; 
     [SerializeField] private Button continueButton; 
-    [SerializeField] private TMP_text gameOverText;
+    [SerializeField] private TMP_Text gameOverText;
     [SerializeField] private ScoreSystem scoreSystem; 
     [SerializeField] private GameObject gameOverDisplay;
     [SerializeField] private AsteroidSpawner asteroidSpawner;
@@ -17,26 +18,27 @@ public class NewBehaviourScript : MonoBehaviour
     {
         asteroidSpawner.enabled = false;
 
-         int finalScore = scoreSystem.EndTimer();
-         gameOverText.text = $"Your Score: {finalScore}";
+        int finalScore = scoreSystem.EndTimer();
+        gameOverText.text = $"Your Score: {finalScore}";
 
         gameOverDisplay.gameObject.SetActive(true);
     }
+
     public void PlayAgain()
     {
         SceneManager.LoadScene(1);
     }
 
-    public void ContinueGame()
-    {
-        AdManager.Instance.ShowAd(this);
-
-        continueButton.interactable = false; 
-    }
-
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void ContinueButton()
+    {
+        AdManager.Instance.ShowAd(this);
+
+        continueButton.interactable = false;
     }
 
     public void ContinueGame()
@@ -45,11 +47,11 @@ public class NewBehaviourScript : MonoBehaviour
 
         player.transform.position = Vector3.zero; 
         player.SetActive(true); 
-        player.GetComponent<RigidBody>().velocity = Vector3.zero; 
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero; 
 
         asteroidSpawner.enabled = true; 
 
         gameOverDisplay.gameObject.SetActive(false);
+
     }
 }
-
